01fac71f0cbe5a1b72876bc877597ac8bdc598bb
j985chen <j985chen@uwaterloo.ca>
2021-07-11 00:45:00 -0400

    added popup to configure censorship settings

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
 chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({censorLevel: 'word'}, function() {
+    console.log(`The storage level is word`);
+  });
   console.log("Successfully installed");
 });
