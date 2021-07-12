2e7b4456b6866d9d271ec715c3799866bf81a307
j985chen <j985chen@uwaterloo.ca>
2021-07-11 23:32:06 -0400

    added functions to detect tab changes

--- background.js
+++ background.js
@@ -7,3 +7,26 @@ chrome.runtime.onInstalled.addListener(function() {
   );
   console.log("Successfully installed");
 });
+
+chrome.tabs.query({ active: true }, function(tab) {
+  getStrongLanguage(tab.url);
+});
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  chrome.tabs.query({ active: true }, function(tab) {
+    getStrongLanguage(tab[0].url);
+  });
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  getStrongLanguage(tab.url);
+});
+
+chrome.tabs.onCreated.addListener(function(tab) { 
+  console.log(document);
+  getStrongLanguage(tab.url);
+});
+
+function getStrongLanguage(url) {
+  console.log(url);
+}
