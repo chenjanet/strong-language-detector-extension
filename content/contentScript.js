78043c3dc5c6e990a8d756611adef57589f78888
j985chen <j985chen@uwaterloo.ca>
2021-07-14 11:58:15 -0400

    added functions to process words, sentences, and page text in content script

--- content/contentScript.js
+++ content/contentScript.js
@@ -1,5 +1,32 @@
+const sentenceReg = new RegExp("[^\.!\?\n]+[\.!\?\n]+", "g");
+const wordReg = new RegExp("[A-Za-z0-9_]+", "g");
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        console.log(request.url);
+        chrome.storage.sync.get('censorLevel', function(data) {
+            if (data.censorLevel == "word") {
+                censorWords();
+            } else if (data.censorLevel == "sentence") {
+                censorSentences();
+            } else {
+                censorPage();
+            }
+            console.log("url", request.url);
+        }); 
     }
 );
+
+function censorWords() {
+    const words = new Set(document.body.innerText.match(wordReg));
+    console.log(words);
+}
+
+function censorSentences() {
+    const sentences = new Set(document.body.innerText.match(sentenceReg));
+    console.log(sentences);
+} 
+
+function censorPage() {
+    const page = document.body.innerText;
+    console.log(page);
+}
