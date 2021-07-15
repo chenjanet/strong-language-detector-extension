f1b4324c9324177625f921a408d60495b304a803
j985chen <j985chen@uwaterloo.ca>
2021-07-14 22:38:18 -0400

    implemented obtaining strong language via api

--- content/contentScript.js
+++ content/contentScript.js
@@ -1,34 +1,26 @@
 const sentenceReg = new RegExp("[^\.!\?\n]+[\.!\?\n]+", "g");
 const wordReg = new RegExp("[A-Za-z0-9_]+", "g");
 
-const censorWords = () => {
-    const words = new Set(document.body.innerText.match(wordReg));
-    words.forEach((element, index) => {
+const wordsList = () => { return Array.from(new Set(document.body.innerText.match(wordReg))); }
 
-    });
-    console.log(words);
-};
+const sentencesList = () => { return Array.from(new Set(document.body.innerText.match(sentenceReg))); }
 
-const censorSentences = () => {
-    const sentences = new Set(document.body.innerText.match(sentenceReg));
-    console.log(sentences);
-} 
-
-const censorPage = () => {
-    const page = document.body.innerText;
-    console.log(page);
-}
+const pageText = () => { return [ document.body.innerText ] };
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-        chrome.storage.sync.get('censorLevel', function(data) {
-            if (data.censorLevel == "word") {
-                censorWords();
-            } else if (data.censorLevel == "sentence") {
-                censorSentences();
-            } else {
-                censorPage();
-            }
-            console.log("url", request.url);
-        }); 
-    }
-);
+    chrome.storage.sync.get('censorLevel', function(data) {
+        let texts;
+        if (data.censorLevel == "word") {
+            texts = wordsList();
+        } else if (data.censorLevel == "sentence") {
+            texts = sentencesList();
+        } else {
+            texts = pageText();
+        }
+        chrome.runtime.sendMessage({ message: 'get_strong_language', data: { "text": Array.from(texts) } }, (response) => {
+            texts = texts.filter((word, index) => { return response.data.is_strong[index] === 1 });
+            console.log(texts);
+        });
+    }); 
+    
+});
