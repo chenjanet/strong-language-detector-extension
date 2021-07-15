dc1d0089e2543c51a56d2ae43589c510688b4a27
j985chen <j985chen@uwaterloo.ca>
2021-07-14 22:37:18 -0400

    added script to call API for strong language

--- background.js
+++ background.js
@@ -7,7 +7,7 @@ chrome.runtime.onInstalled.addListener(function() {
   );
   chrome.tabs.query({ active: true }, (tabs) => {
     if (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { "message": "activated_tab", "url": tabs[0].url });
+      chrome.tabs.sendMessage(tabs[0].id, { "message": "tab_change", "url": tabs[0].url });
     }
   });
 });
@@ -15,17 +15,46 @@ chrome.runtime.onInstalled.addListener(function() {
 chrome.tabs.onActivated.addListener((activeInfo) => {
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     if (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { "message": "activated_tab", "url": tabs[0].url });
+      chrome.tabs.sendMessage(tabs[0].id, { "message": "tab_change", "url": tabs[0].url });
     }
   });
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === "complete") {
-    chrome.tabs.sendMessage(tabId, { "message": "updated_tab", "url": tab.url });
+    chrome.tabs.sendMessage(tabId, { "message": "tab_change", "url": tab.url });
   }
 });
 
 chrome.tabs.onCreated.addListener((tab) => {
-  chrome.tabs.sendMessage(tab.id, { "message": "created_tab", "url": tab.url });
+  chrome.tabs.sendMessage(tab.id, { "message": "tab_change", "url": tab.url });
 });
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  switch (request.message) {
+    case 'get_strong_language': {
+      postData("http://localhost:5000/is_strong", request.data)
+        .then(data => {
+          sendResponse({ data });
+        });
+    }
+  }
+  return true;
+});
+
+const postData = async (url = '', data = {}) => {
+  // Default options are marked with *
+  const response = await fetch(url, {
+      method: 'POST',
+      mode: 'cors', 
+      cache: 'no-cache', 
+      credentials: 'same-origin', 
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      redirect: 'follow', 
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify(data)
+  });
+  return response.json();
+}
